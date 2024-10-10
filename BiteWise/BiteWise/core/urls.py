94ae7422c28377a0cd84a7d7e5e043e62ae43f46
Luhcyy <super.ghast8@gmail.com>
2024-10-09 21:44:32 -0300

    Feat: add login and signup routes

--- BiteWise/BiteWise/core/urls.py
+++ BiteWise/BiteWise/core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.signup),
+    path('cadastro', views.signup, name='signup'),
+    path('login', views.login, name='login')
 ]
