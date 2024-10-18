5eb7d6b87b740feeda5bac75f73a79b20bbadccc
Lucas Luiz Assis <83933766+Luhcyy@users.noreply.github.com>
2024-10-18 08:26:27 -0300

    Remove: Delete not working BiteWise directory

--- BiteWise/BiteWise/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('cadastro', views.signup, name='signup'),
-    path('login', views.login, name='login'),
-    path('', views.home, name='home')
-]
