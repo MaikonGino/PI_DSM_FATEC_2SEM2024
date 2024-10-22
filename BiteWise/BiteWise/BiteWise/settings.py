ee85b5eff3495f6d83557d644c3a6f8477ecdc0f
Luhcyy <super.ghast8@gmail.com>
2024-10-22 09:10:26 -0300

    Feat: Configure SQLite for dynamic data

--- BiteWise/BiteWise/BiteWise/settings.py
+++ BiteWise/BiteWise/BiteWise/settings.py
@@ -128,3 +128,5 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- BiteWise/BiteWise/core/models.py
+++ BiteWise/BiteWise/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Ingrediente(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    imagem = models.ImageField(upload_to='ingredientes/')
+
+    def __str__(self):
+        return self.nome
